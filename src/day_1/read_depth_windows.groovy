2e77c9cc25cb8f1623a64b2f0b6a4630b4cce0b6
Greg Rushton <grushton@broadinstitute.org>
2021-12-01 12:13:07 -0500

    simplify

---
+++ src/day_1/read_depth_windows.groovy
@@ -0,0 +1,36 @@
+package day_1
+
+static void main(String[] args) {
+    println("Reading Depth Windows")
+    File file = new File("./depths.txt")
+    List<String> depths = file.readLines()
+    List<Integer> depthInts = depths.collect { String it -> Integer.valueOf(it) }
+    List<Tuple3<Integer, Integer, Integer>> tuples = new ArrayList<>()
+    depthInts.eachWithIndex { int entry, int i ->
+        {
+            if (i < 2) {
+                // do nothing with rows 0 and 1.
+            } else {
+                // Construct a new triple from the previous 3 elements
+                Tuple3<Integer, Integer, Integer> tuple = new Tuple3(depthInts[i - 2], depthInts[i - 1], entry)
+                tuples.add(tuple)
+            }
+        }
+    }
+    int increaseCounter = 0
+    tuples.eachWithIndex { Tuple3 tuple, int i ->
+        int sum = tuple.getV1() + tuple.getV2() + tuple.getV3()
+        if (i == 0) {
+            println(sum + " (N/A - no previous sum)")
+        } else {
+            int previousSum = tuples[i - 1].getV1() + tuples[i - 1].getV2() + tuples[i - 1].getV3()
+            if (sum > previousSum) {
+                println(sum + " (increased)")
+                increaseCounter++
+            } else {
+                println(sum + " (decreased)")
+            }
+        }
+    }
+    println("Final number of increases: " + increaseCounter)
+}
--- src/day_1/read_depths.groovy
+++ src/day_1/read_depths.groovy
@@ -1,25 +1,22 @@
 package day_1
 
-class readDepths {
-    static void main(String[] args) {
-        println("Reading Depths")
-        File file = new File("./src/day_1/depths.txt")
-        List<String> depths = file.readLines()
-        List<Integer> depthInts = depths.collect { String it -> Integer.valueOf(it)}
-        int increaseCounter = 0
-        depths.eachWithIndex { String entry, int i -> 
-            if (i == 0) {
-                println(entry + " (N/A - no previous measurement)")
+static void main(String[] args) {
+    println("Reading Depths")
+    File file = new File("./depths.txt")
+    List<String> depths = file.readLines()
+    List<Integer> depthInts = depths.collect { String it -> Integer.valueOf(it) }
+    int increaseCounter = 0
+    depths.eachWithIndex { String entry, int i ->
+        if (i == 0) {
+            println(entry + " (N/A - no previous measurement)")
+        } else {
+            if (depthInts[i] > depthInts[i - 1]) {
+                println(entry + " (increased)")
+                increaseCounter++
             } else {
-                if (depthInts[i] > depthInts[i-1]) {
-                    println(entry + " (increased)")
-                    increaseCounter++
-                } else {
-                    println(entry + " (decreased)")
-                }
+                println(entry + " (decreased)")
             }
-        } 
-        println("Final number of increases: " + increaseCounter)
+        }
     }
+    println("Final number of increases: " + increaseCounter)
 }
-
