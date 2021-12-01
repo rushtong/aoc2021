936609e8b77a89e60cfc307d09a3344c9d25be14
Greg Rushton <grushton@broadinstitute.org>
2021-12-01 12:07:05 -0500

    use tuples

--- src/day_1/readDepthWindows.groovy
+++ src/day_1/readDepthWindows.groovy
@@ -6,43 +6,32 @@ class readDepthWindows {
         File file = new File("./src/day_1/depths.txt")
         List<String> depths = file.readLines()
         List<Integer> depthInts = depths.collect { String it -> Integer.valueOf(it)}
-        List<Triple> triples = new ArrayList<>() 
+        List<Tuple3<Integer, Integer, Integer>> tuples = new ArrayList<>() 
         depthInts.eachWithIndex { int entry, int i -> {
                 if (i < 2) {
                     // do nothing with rows 0 and 1.
                 } else {
                     // Construct a new triple from the previous 3 elements
-                    Triple triple = new Triple()
-                    triple.setIndex(i)
-                    triple.setA(depthInts[i-2])
-                    triple.setB(depthInts[i-1])
-                    triple.setC(entry)
-                    triples.add(triple)
+                    Tuple3<Integer, Integer, Integer> tuple = new Tuple3(depthInts[i-2], depthInts[i-1], entry)
+                    tuples.add(tuple)
                 }
             }
-        } 
+        }
         int increaseCounter = 0
-        triples.eachWithIndex { Triple triple, int i ->
+        tuples.eachWithIndex { Tuple3 tuple, int i ->
+            int sum = tuple.getV1() + tuple.getV2() + tuple.getV3()
             if (i == 0) {
-                println(triple.sum() + " (N/A - no previous sum)")
+                println(sum + " (N/A - no previous sum)")
             } else {
-                if (triples[i].sum() > triples[i-1].sum()) {
-                    println(triple.sum() + " (increased)")
+                int previousSum = tuples[i-1].getV1() + tuples[i-1].getV2() + tuples[i-1].getV3()
+                if (sum > previousSum) {
+                    println(sum + " (increased)")
                     increaseCounter++
                 } else {
-                    println(triple.sum() + " (decreased)")
+                    println(sum + " (decreased)")
                 }
             }
         } 
         println("Final number of increases: " + increaseCounter)
     }
-}
-
-class Triple {
-    int index
-    int A
-    int B
-    int C
-    int sum() { A + B + C }
-    String toString() {  "index: " + index + " A: " + A + " B: " + B + " C: " + C + " sum: " + sum() }
 }
