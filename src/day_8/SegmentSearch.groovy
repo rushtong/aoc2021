0a1b5f0f9b0128a43b7e0925e87f0bfde9678317
Greg Rushton <grushton@broadinstitute.org>
2021-12-08 06:06:56 -0500

    cleanup for the setup

--- src/day_8/SegmentSearch.groovy
+++ src/day_8/SegmentSearch.groovy
@@ -7,7 +7,10 @@ class SegmentSearch {
         boolean partOne = true
         println("Seven Segment Search")
         File file = train ? new File("./src/day_8/train.txt") : new File("./src/day_8/input.txt")
-        println(file.readLines())
+        file.readLines().each {line -> {
+            String[] halves = line.split("\\|").collect {it.trim()}
+            println(halves)
+        }}
     }
 
 }
