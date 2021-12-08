963b2399d035bb9d48a9e256cca3bf247e06bb97
Greg Rushton <grushton@broadinstitute.org>
2021-12-08 06:28:08 -0500

    part 1 and instructions for part 2

--- src/day_8/SegmentSearch.groovy
+++ src/day_8/SegmentSearch.groovy
@@ -3,14 +3,22 @@ package day_8
 class SegmentSearch {
 
     static void main(String[] args) {
-        boolean train = true
-        boolean partOne = true
+        boolean train = false
+        boolean partOne = true // 519
         println("Seven Segment Search")
         File file = train ? new File("./src/day_8/train.txt") : new File("./src/day_8/input.txt")
-        file.readLines().each {line -> {
-            String[] halves = line.split("\\|").collect {it.trim()}
-            println(halves)
-        }}
+        String[] matchingSegments = SegmentSearch.partOne(file);
+        println("Final result: " + matchingSegments)
+        println("Final count: " + matchingSegments.size())
     }
 
+    // digit segments for 1, 4, 7, and 8 are segment sizes 2, 4, 3, and 7 respectively and uniquely
+    // Count those up ...
+    static final String[] partOne(File file) {
+        file.readLines().collect {line -> {
+            String[] halves = line.split("\\|").collect {it.trim()}
+            String[] segments = halves.last().split("\\s")
+            segments.findAll {[2, 4, 3, 7].contains(it.length())}
+        }}.flatten()
+    }
 }
