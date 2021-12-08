49fc0180653b663a49efbe397d1974878b0fbb69
Greg Rushton <grushton@broadinstitute.org>
2021-12-07 19:00:30 -0500

    more optimizations

--- src/day_6/LanternFish2.groovy
+++ src/day_6/LanternFish2.groovy
@@ -1,6 +1,11 @@
 package day_6
 
-// OOM's around 184 with this version
+import groovy.transform.CompileStatic
+import groovy.transform.TailRecursive
+import groovy.transform.TypeChecked
+
+@CompileStatic
+@TypeChecked
 class LanternFish2 {
     static void main(String[] args) {
         boolean train = true
@@ -10,27 +15,39 @@ class LanternFish2 {
         Collection<Integer> fish = file.readLines().get(0).split(',').collect {Integer.valueOf(it) }
         println("Initial State (" + fish.size() + "): " + fish)
 
-        // Do the calculations only once for a fish of initial count 1 (there are lots of them)
-        // Then, just multiply the counts by the number of times the initial count
-        // appears in the initial list
+        // Do the traversal calculations only once for initial count 1 (there are lots of them)
+        // Calculate the value for anything above that manually (2 is just 1 more than the count for 1, etc)
+        // Lastly, sum up the matching count for each value in the input file for a total
 
+//  export JAVA_OPTS="$JAVA_OPTS -Xmx8000M" gets me to 205-ish
+// ➜  aoc2021 git:(main) ✗ export JAVA_OPTS="$JAVA_OPTS -Xmx10000M" groovy ./src/day_6/LanternFish2.groovy
+// ➜  Gets me to around ...
         Map<Integer, Integer> spawnStateMap = new HashMap<>()
-        def intRange = 1..fish.max()
-        intRange.each {spawnState ->
-            def tempState = spawnFactorial(256, [spawnState])
-            spawnStateMap.put(spawnState, tempState.size())
-        }
+        def spawnState1 = spawnFactorial(256, [1])
+//        // Spawn rates of anything more than 1 is just one additional prepended array element
+        spawnStateMap.put(1, spawnState1.size())
+        spawnStateMap.put(2, spawnState1.size() + 1)
+        spawnStateMap.put(3, spawnState1.size() + 2)
+        spawnStateMap.put(4, spawnState1.size() + 3)
+        spawnStateMap.put(5, spawnState1.size() + 4)
+//        // After 6, there are 2 additional prepended array elements since that's when we spawn
+        spawnStateMap.put(6, spawnState1.size() + 8)
+        spawnStateMap.put(7, spawnState1.size() + 9)
+        spawnStateMap.put(8, spawnState1.size() + 10)
+        spawnStateMap.put(9, spawnState1.size() + 11)
+
         println(spawnStateMap)
     }
 
+    @TailRecursive
     static List<Integer> spawnFactorial(int day, List<Integer> accumulator) {
-        println("Day: " + (256 - day + 1) + " size:" + accumulator.size())
+        println("Day: " + day)
         day == 1 ?
                 accumulator :
                 spawnFactorial(day - 1, accumulator.collect {spawn(it)}.flatten() as List<Integer>)
     }
 
-    static int[] spawn(int n) {
+    static List<Integer> spawn(int n) {
         n == 0 ? [6, 8] : [n - 1]
     }
 
